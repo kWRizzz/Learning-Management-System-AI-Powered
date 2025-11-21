d2b01558a83fa80b9953d0fbb59765f10f8a5f79
kWRizzz <krishnathey2j@gmail.com>
2025-11-21 23:41:39 +0530

    Made Some new Resposive Update On Frontend and in backend also

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -3,8 +3,16 @@ import userModel from "../models/user.model";
 const getCurrentUser= async (req,res)=>{
     try {
         const user=await userModel.findById(req.userId).select("-password")
+        if(!user) return res.status(404).json({
+            message:"User is not found"
+        })
+        return res.status(200).json(user)
     } catch (error) {
+        console.log(error);
         
+        return res.status(500).json({
+            message:`GetCurrent User errror =>${error}`
+        })
     }
 }
 
